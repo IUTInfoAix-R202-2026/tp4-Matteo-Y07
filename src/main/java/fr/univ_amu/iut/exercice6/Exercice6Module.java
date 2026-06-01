e750d43a9b133725ff0cea2b1af2bcf376625af0
Mattéo Yanni <matteo.yanni@etu.univ-amu.fr>
2026-06-01 08:49:05 +0000

    Exercice 6 terminé

--- src/main/java/fr/univ_amu/iut/exercice6/Exercice6Module.java
+++ src/main/java/fr/univ_amu/iut/exercice6/Exercice6Module.java
@@ -1,6 +1,7 @@
 package fr.univ_amu.iut.exercice6;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 /**
  * Module Guice de l'exercice 6.
@@ -18,8 +19,10 @@ public class Exercice6Module extends AbstractModule {
   protected void configure() {
     // TODO exercice 6 : lier chaque implémentation de Notifieur à un nom.
     //
-    // - le nom "console"    -> NotifieurConsole
+    // - le nom "console" -> NotifieurConsole
+    bind(Notifieur.class).annotatedWith(Names.named("console")).to(NotifieurConsole.class);
     // - le nom "silencieux" -> NotifieurSilencieux
+    bind(Notifieur.class).annotatedWith(Names.named("silencieux")).to(NotifieurSilencieux.class);
     // Astuce :
     // bind(Notifieur.class).annotatedWith(Names.named("console")).to(NotifieurConsole.class);
   }
